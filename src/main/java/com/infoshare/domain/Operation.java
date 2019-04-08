a515889d198bf12ef49d3667ae7a9686ac578b32
Arkadiusz Kałka <arkadiusz.kalka@gmail.com>
2019-04-08 15:08:26 +0200

    First step to move to hibernate

--- src/main/java/com/infoshare/domain/Operation.java
+++ src/main/java/com/infoshare/domain/Operation.java
@@ -2,22 +2,55 @@ package com.infoshare.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDate;
 
 @Data
-public class Operation {
+
+@Entity
+@Table(name = "operations")
+public class Operation implements Serializable
+{
+    private static final long serialVersionUID = -2122250031742176764L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+]    @Column
     private int userId;
+
+    @Column
     private String userName;
+
+    @Column
     private int bookId;
+
+    @Column
     private String bookTitle;
+
+    @Column
     private String author;
+
+    @Column
     private LocalDate operationDate;
+
+    @Column
     private LocalDate startDate;
+
+    @Column
     private LocalDate endDate;
+
+    @Enumerated(EnumType.STRING)
     private OperationType operationType;
 
-    public Operation( int id, int userId, String userName, int bookId, String bookTitle,
+
+    public Operation() {
+        //for JPA
+    }
+
+    public Operation(int id, int userId, String userName, int bookId, String bookTitle,
                      String author, LocalDate operationDate, LocalDate startDate,
                      LocalDate endDate, OperationType operationType) {
 
@@ -33,9 +66,9 @@ public class Operation {
         this.operationType = operationType;
     }
 
-    public Operation( int userId, String userName, int bookId, String bookTitle,
-                      String author, LocalDate operationDate, LocalDate startDate,
-                      LocalDate endDate, OperationType operationType) {
+    public Operation(int userId, String userName, int bookId, String bookTitle,
+                     String author, LocalDate operationDate, LocalDate startDate,
+                     LocalDate endDate, OperationType operationType) {
 
         this.userId = userId;
         this.userName = userName;
@@ -47,4 +80,5 @@ public class Operation {
         this.endDate = endDate;
         this.operationType = operationType;
     }
+
 }
