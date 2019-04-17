8bf76e45e5b06fbdb83fbd077ddc18fd81854953
Arkadiusz Kałka <arkadiusz.kalka@gmail.com>
2019-04-17 15:52:54 +0200

    Delete unused method in OperationQuery

--- src/main/java/com/infoshare/query/OperationsQuery.java
+++ src/main/java/com/infoshare/query/OperationsQuery.java
@@ -1,28 +1,13 @@
 package com.infoshare.query;
 
-import com.infoshare.domain.Basket;
-import com.infoshare.domain.OperationType;
-import com.infoshare.domain.User;
-import com.infoshare.utils.DateUtil;
-
-
-import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
-import java.util.List;
 
 import static com.infoshare.dao.DBCon.preparedStatement;
 
 public class OperationsQuery {
 
-    String query = "SELECT * FROM `operation` " +
-            "JOIN `user` ON operation.userId = user.id " +
-            "JOIN book ON operation.bookId=book.id " +
-            "WHERE user.id = 10";
-
-
-    public static ResultSet listOfOperationsByUserId(int userID) throws SQLException, ClassNotFoundException {
+      public static ResultSet listOfOperationsByUserId(int userID) throws SQLException, ClassNotFoundException {
 
         String query = "SELECT * FROM operation WHERE userID = " + userID;
         return preparedStatement(query).executeQuery();
@@ -45,30 +30,5 @@ public class OperationsQuery {
         else query += "WHERE 1";
 
         return preparedStatement(query).executeQuery();
-    }
-
-    public static ResultSet listOfBorrowedBookByUserId(int userID) throws SQLException, ClassNotFoundException {
-        String endDate = "1970.01.01";
-        String query = "SELECT * FROM operation " +
-                "JOIN `user` ON operation.userId = user.id " +
-                "JOIN book ON operation.bookId=book.id " +
-                "WHERE userID = " + userID + " " +
-                "AND endDate = '" + endDate + "'";
-        return preparedStatement(query).executeQuery();
-    }
-
-    public static void ReturnBook(int id, int bookId, LocalDate endDate) {
-        String endDateString = endDate.toString();
-        // UPDATE `librarydb`.`operations` SET `endDate`='1970-01-02' WHERE `id`='25';
-        String query = "UPDATE operation SET endDate='" + endDateString + "' WHERE id=" + id;
-        String statusAvailableQuery = "UPDATE book SET status='Dostępna' WHERE id=" + bookId;
-        try {
-            preparedStatement(query).execute();
-            preparedStatement(statusAvailableQuery).execute();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
     }
 }
