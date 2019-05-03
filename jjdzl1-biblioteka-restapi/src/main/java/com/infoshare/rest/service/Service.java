a411251314fdf3ea12057c7c8b9349137c4beb99
Arkadiusz Kałka <arkadiusz.kalka@gmail.com>
2019-05-03 12:33:30 +0200

    REST now display books list al JSON

--- jjdzl1-biblioteka-restapi/src/main/java/com/infoshare/rest/service/Service.java
+++ jjdzl1-biblioteka-restapi/src/main/java/com/infoshare/rest/service/Service.java
@@ -1,6 +1,8 @@
 package com.infoshare.rest.service;
 
+import com.infoshare.logic.domain.Book;
 import com.infoshare.logic.domain.User;
+import com.infoshare.logic.repository.BooksRepositoryDao;
 import com.infoshare.logic.repository.UsersRepositoryDao;
 
 import javax.ejb.EJB;
@@ -9,6 +11,7 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import java.io.FileNotFoundException;
 import java.sql.SQLException;
 import java.util.Collection;
 
@@ -18,6 +21,8 @@ public class Service {
     @EJB
     private UsersRepositoryDao usersRepository;
 
+    @EJB
+    private BooksRepositoryDao booksRepository;
 
     @Context
     private UriInfo uriInfo;
@@ -34,4 +39,17 @@ public class Service {
         }
         return Response.ok(users).build();
     }
+
+    @GET
+    @Path("/books/{page}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getBooks(@PathParam("page") String page) throws SQLException, ClassNotFoundException, FileNotFoundException {
+
+        Collection<Book> books = booksRepository.bookList("title", Integer.parseInt(page));
+        if (books.isEmpty()) {
+            return Response.noContent().build();
+        }
+        return Response.ok(books).build();
+    }
+
 }
